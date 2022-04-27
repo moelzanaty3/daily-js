95b5555f24634f1900f01f2deb63b243969388ca
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-04-27 11:22:09 +0200

    lesson 1: arrow function

---
+++ lessons/arrow-functions.js
@@ -0,0 +1,11 @@
+// 27 Apr 2022 - Mohammed Elzanaty
+
+// 💡 Arrow function expressions 💡
+
+// Traditional Anonymous Function
+function func(a, b) {
+  return a + b + 100
+}
+
+// Arrow Function
+const func = (a, b) => a + b + 100
